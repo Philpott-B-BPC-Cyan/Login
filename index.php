7a6189e0244b1690543414ce406fe2a4859dc139
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-11-24 09:42:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <html>
 <body>
 
-<form action - "login.php" method-"post">
-Username<input type="text" name="username">
-Password<input type="password" name="password">
-<input type="submit" value="Login">
-</form>	
-
 <?php
 
 $uname=$_POST['username'];
